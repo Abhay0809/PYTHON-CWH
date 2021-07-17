a11100664d55d73fb1dd8dd43cf385a5fb3063b1
Abhay0809 <48680107+Abhay0809@users.noreply.github.com>
2021-07-17 09:54:27 +0530

    Practice Set 2

---
+++ Practice_Set_02.py
@@ -0,0 +1,29 @@
+# Python Program for sum of two number
+a = input("Enter first Number: ")
+b = input("Enter second Number: ")
+print("Sum is", (int(a)+int(b)))
+
+# Python program for quotient and remainder
+c = input("Enter Divisor: ")
+d = input("Enter Dividend: ")
+print("Quotient:", int(c) / int(d))
+print("Remainder:", int(c) % int(d))
+
+# Python program to check type of variable in input function
+temp = input("Enter something to check it's datatype: ")
+print("Type of Input Function is", type(temp))
+
+# Greater of two number using comparison operator
+num1 = 34
+num2 = 80
+print(num1 > num2)
+
+# Python program to find average of two number
+n1 = input("Enter first Number: ")
+n2 = input("Enter second Number: ")
+print("Average =", ((int(n1)+int(n2))/2))
+
+# Python program to find square of number
+square = input("Enter Number: ")
+print("Square of given number is", (int(square)*int(square)))
+
