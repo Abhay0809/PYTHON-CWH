3156582f76529df3c9a85f88669e9e4822e2867b
Abhay0809 <48680107+Abhay0809@users.noreply.github.com>
2021-07-17 10:12:09 +0530

    Strings

---
+++ 06_Strings.py
@@ -0,0 +1,32 @@
+# STRING IS SEQUENCE OF CHARACTER
+
+a = 'Abhay said "he is alone"'
+b = "Abhay's room"
+c = '''Abhay's Room said "Do Not Enter"'''
+
+print(c)
+# print(type(c))
+
+# greeting = "Good Morning, "
+# name = "Abhay"
+# print(type(name))
+
+# Concatenating two strings
+# d = greeting + name
+# print(d)
+name = "Abhay"
+# print(name[4])
+# name[3] = "d" --> Does not work
+
+# print(name[1:4])
+# print(name[:4]) # is same as name[0:4]
+# print(name[1:]) # is same as name[1:5]
+# e = name[-4:-1] # is same is name[1:4]
+# print(e)
+
+name = "AbhayIsGood"
+# f = name[0::3]
+f = name[:0:-1]
+print(f)
+
+
