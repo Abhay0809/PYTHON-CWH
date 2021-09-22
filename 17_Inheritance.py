e0e1baa00240ff0c2b6bd0b4f4c093bfaa6b9b71
Abhay0809 <abhayanandkk23@gmail.com>
2021-09-22 13:33:16 +0530

    Inheritance

--- 17_Inheritance.py
+++ 17_Inheritance.py
@@ -117,7 +117,7 @@ print(pr.country)
 '''
 
 # Super Method
-'''
+
 class Person:
     country = "India"
 
@@ -136,7 +136,7 @@ class Employee(Person):
 
     def getSalary(self):
         print(f"Salary is {self.salary}")
-    
+
     def takeBreath(self):
         super().takeBreath()
         print("I am an Employee so I am luckily breathing..")
@@ -150,108 +150,17 @@ class Programmer(Employee):
 
     def getSalary(self):
         print(f"No salary to programmers")
-    
+
     def takeBreath(self):
         super().takeBreath()
         print("I am a Progarmmer so I am breathing++..")
 
 # p = Person()
-# p.takeBreath() 
+# p.takeBreath()
 
 # e = Employee()
-# e.takeBreath() 
+# e.takeBreath()
 
 pr = Programmer()
-# pr.takeBreath() 
-'''
+# pr.takeBreath()
 
-# Class Method
-'''
-class Employee:
-    company = "Camel"
-    salary = 100
-    location = "Delhi"
-
-    # def changeSalary(self, sal):
-    #     self.__class__.salary = sal
-
-    @classmethod
-    def changeSalary(cls, sal):
-        cls.salary = sal
-
-e = Employee()
-print(e.salary)
-e.changeSalary(455)
-print(e.salary)
-print(Employee.salary)
-'''
-
-# Property decorator
-'''
-class Employee:
-    company = "Bharat Gas"
-    salary = 5600
-    salarybonus = 400
-    # totalSalary = 6100
-
-    @property
-    def totalSalary(self):
-        return self.salary + self.salarybonus
-
-    @totalSalary.setter
-    def totalSalary(self, val):
-        self.salarybonus = val - self.salary
-
-e = Employee()
-print(e.totalSalary)
-e.totalSalary = 5800
-print(e.salary)
-print(e.salarybonus)
-'''
-
-# Operator Overloading
-'''
-class Number:
-    def __init__(self, num):
-        self.num = num
-
-    def __add__(self, num2):
-        print("Lets add")
-        return self.num + num2.num
-
-    def __mul__(self, num2):
-        print("Lets multiply")
-        return self.num * num2.num
-
-n1 = Number(4)
-n2 = Number(6)
-sum = n1 + n2
-mul = n1 * n2
-print(sum)
-print(mul)
-'''
-
-
-# Other Dunder Methods
-class Number:
-    def __init__(self, num):
-        self.num = num
-
-    def __add__(self, num2):
-        print("Lets add")
-        return self.num + num2.num
-
-    def __mul__(self, num2):
-        print("Lets multiply")
-        return self.num * num2.num
-
-    def __str__(self):
-        return f"Decimal Number: {self.num}"
-
-    def __len__(self):
-        return 1
-
-
-n = Number(9)
-print(n)
-print(len(n))
